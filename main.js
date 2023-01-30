73df71887a8dc77b66e24c0a7089646eb1fb93b3
Marley Ferguson <marleyferguson@Marleys-MacBook-Pro.local>
2023-01-30 16:30:12 -0500

    finished

--- main.js
+++ main.js
@@ -1,46 +1,9 @@
 
-// //print
-// console.log("hello");
+let num = 0; 
 
-// //variables
-
-// //
-// const myCourse = "DS4200";
-// console.log(myCourse);
-
-// //myCourse = "DS2000";
-
-// let season = "Winter";
-// console.log(season);
-
-// season = "Spring";
-// console.log(season);
-
-// //var x = 7;
-
-// console.log(typeof(season));
-
-// let hwDue = true;
-// console.log(typeof(hwDue));
-
-// let faveNum = 6;
-// console.log(typeof(faveNum));
-
-// faveNum = "six"
-// console.log(typeof(faveNum));
-
-// function tenTimes(num) {
-// 	let result = num * 10;
-// 	return result;
-// }
-
-function buttonClicked(num) {
-	console.log("button was clicked");
-
-	let buttonDiv = document.getElementById("button-div");
-	buttonDiv.innerHTML = "Number of times button clicked: "+ num + 1;
-}
-
-let ans = tenTimes(6);
-
-console.log(ans);
+function buttonClicked(){ 
+	num = num + 1; 
+	let newText = "button was clicked " + num + " times"; 
+	//select the element 
+	let buttonDiv = document.getElementById("button-div"); 
+	buttonDiv.innerHTML = newText; 
